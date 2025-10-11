1b13831c1a28e2d944f282e08eecb1bd7d735399
sillysillman <sillysillyman.cs@gmail.com>
2025-10-12 00:11:25 +0900

    feat: EnableWebSecurity 애너테이션 추가

--- src/main/kotlin/io/waggle/waggleapiserver/security/config/SecurityConfig.kt
+++ src/main/kotlin/io/waggle/waggleapiserver/security/config/SecurityConfig.kt
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
@@ -16,6 +17,7 @@ import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig(
     private val jwtAuthenticationFilter: JwtAuthenticationFilter,
     private val customOAuth2UserService: CustomOAuth2UserService,
