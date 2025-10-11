27f0d8dc65c997eb9ee1c43580288716e8a4fb5f
sillysillman <sillysillyman.cs@gmail.com>
2025-10-12 02:15:54 +0900

    feat: 프로젝트명 중복 검사 추가

--- src/main/kotlin/io/waggle/waggleapiserver/domain/project/repository/ProjectRepository.kt
+++ src/main/kotlin/io/waggle/waggleapiserver/domain/project/repository/ProjectRepository.kt
@@ -3,4 +3,6 @@ package io.waggle.waggleapiserver.domain.project.repository
 import io.waggle.waggleapiserver.domain.project.Project
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ProjectRepository : JpaRepository<Project, Long>
+interface ProjectRepository : JpaRepository<Project, Long> {
+    fun existsByName(name: String): Boolean
+}
