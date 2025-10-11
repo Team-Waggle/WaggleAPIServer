0012653ae876126928223c22cf4ac33f8745cd66
sillysillman <sillysillyman.cs@gmail.com>
2025-10-12 01:47:48 +0900

    feat: MemberRepository 추가

---
+++ src/main/kotlin/io/waggle/waggleapiserver/domain/member/repository/MemberRepository.kt
@@ -0,0 +1,12 @@
+package io.waggle.waggleapiserver.domain.member.repository
+
+import io.waggle.waggleapiserver.domain.member.Member
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.UUID
+
+interface MemberRepository : JpaRepository<Member, Long> {
+    fun findByUserIdAndProjectId(
+        userId: UUID,
+        projectId: Long,
+    ): Member?
+}
