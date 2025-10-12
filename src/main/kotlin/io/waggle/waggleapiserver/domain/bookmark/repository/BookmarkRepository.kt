b86ca7263acae8cfe676420218818616a61273e7
sillysillman <sillysillyman.cs@gmail.com>
2025-10-13 00:00:45 +0900

    fix: ID 타입 변경

--- src/main/kotlin/io/waggle/waggleapiserver/domain/bookmark/repository/BookmarkRepository.kt
+++ src/main/kotlin/io/waggle/waggleapiserver/domain/bookmark/repository/BookmarkRepository.kt
@@ -1,6 +1,7 @@
 package io.waggle.waggleapiserver.domain.bookmark.repository
 
 import io.waggle.waggleapiserver.domain.bookmark.Bookmark
+import io.waggle.waggleapiserver.domain.bookmark.BookmarkId
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface BookmarkRepository : JpaRepository<Bookmark, Long>
+interface BookmarkRepository : JpaRepository<Bookmark, BookmarkId>
