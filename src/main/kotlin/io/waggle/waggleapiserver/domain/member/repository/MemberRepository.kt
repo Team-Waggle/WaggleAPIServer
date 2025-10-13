76b047e0380bf3fde88a6523bd3498d5db5a805d
sillysillman <sillysillyman.cs@gmail.com>
2025-10-13 23:38:07 +0900

    feat: FETCH JOIN 쿼리 추가

--- src/main/kotlin/io/waggle/waggleapiserver/domain/member/repository/MemberRepository.kt
+++ src/main/kotlin/io/waggle/waggleapiserver/domain/member/repository/MemberRepository.kt
@@ -2,6 +2,8 @@ package io.waggle.waggleapiserver.domain.member.repository
 
 import io.waggle.waggleapiserver.domain.member.Member
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import java.util.UUID
 
 interface MemberRepository : JpaRepository<Member, Long> {
@@ -10,5 +12,29 @@ interface MemberRepository : JpaRepository<Member, Long> {
         projectId: Long,
     ): Member?
 
-    fun findByProjectId(projectId: Long): List<Member>
+    @Query(
+        """
+        SELECT m 
+        FROM Member m 
+        JOIN FETCH m.project 
+        WHERE m.user.id = :userId 
+        ORDER BY m.createdAt ASC
+    """,
+    )
+    fun findAllByUserIdWithProjectOrderByCreatedAtAsc(
+        @Param("userId") userId: Long,
+    ): List<Member>
+
+    @Query(
+        """
+        SELECT m 
+        FROM Member m 
+        JOIN FETCH m.user 
+        WHERE m.project.id = :projectId 
+        ORDER BY m.createdAt ASC
+    """,
+    )
+    fun findAllByProjectIdWithUserOrderByCreatedAtAsc(
+        @Param("projectId") projectId: Long,
+    ): List<Member>
 }
