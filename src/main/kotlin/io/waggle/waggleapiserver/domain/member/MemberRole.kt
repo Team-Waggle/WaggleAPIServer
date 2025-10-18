83ae01754910965e8cd3c5284afff652c342a859
sillysillman <sillysillyman.cs@gmail.com>
2025-10-19 00:40:56 +0900

    chore: 불필요한 프로퍼티 및 메서드 제거

--- src/main/kotlin/io/waggle/waggleapiserver/domain/member/MemberRole.kt
+++ src/main/kotlin/io/waggle/waggleapiserver/domain/member/MemberRole.kt
@@ -2,45 +2,8 @@ package io.waggle.waggleapiserver.domain.member
 
 enum class MemberRole(
     val level: Int,
-    val permissions: Set<Permission>,
 ) {
-    MEMBER(
-        level = 1,
-        permissions = setOf(Permission.CREATE_POST),
-    ),
-    MANAGER(
-        level = 2,
-        permissions =
-            setOf(
-                Permission.CREATE_POST,
-                Permission.MANAGE_MEMBERS,
-                Permission.DELETE_POST,
-            ),
-    ),
-    LEADER(
-        level = 3,
-        permissions =
-            setOf(
-                Permission.CREATE_POST,
-                Permission.MANAGE_MEMBERS,
-                Permission.DELETE_POST,
-                Permission.DELETE_PROJECT,
-                Permission.MODIFY_PROJECT,
-            ),
-    ),
-    ;
-
-    fun hasPermission(permission: Permission): Boolean = permissions.contains(permission)
-
-    fun isHigherThan(other: MemberRole): Boolean = this.level > other.level
-
-    fun isEqualOrHigherThan(other: MemberRole): Boolean = this.level >= other.level
-
-    enum class Permission {
-        CREATE_POST,
-        MANAGE_MEMBERS,
-        DELETE_POST,
-        DELETE_PROJECT,
-        MODIFY_PROJECT,
-    }
+    MEMBER(level = 1),
+    MANAGER(level = 2),
+    LEADER(level = 3),
 }
