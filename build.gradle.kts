ef5002ee76c3e2d590daa5f6ab344457e3c86f2a
sillysillman <sillysillyman.cs@gmail.com>
2025-11-17 21:58:16 +0900

    build: socket 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,13 +36,14 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
 
     // Spring Boot
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
 
     // Kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
