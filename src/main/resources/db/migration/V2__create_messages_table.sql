36adf25e2600bd4d4191fbff7a0d63fc481e76a5
sillysillman <sillysillyman.cs@gmail.com>
2025-11-20 22:15:29 +0900

    feat: index ddl 수정

--- src/main/resources/db/migration/V2__create_messages_table.sql
+++ src/main/resources/db/migration/V2__create_messages_table.sql
@@ -1,13 +1,16 @@
--- Messages table
-CREATE TABLE messages (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    sender_id BINARY(16) NOT NULL,
-    receiver_id BINARY(16) NOT NULL,
-    content TEXT NOT NULL,
-    read_at DATETIME(6),
-    created_at DATETIME(6) NOT NULL,
-    PRIMARY KEY (id),
-    INDEX idx_messages_sender_id (sender_id),
-    INDEX idx_messages_receiver_id (receiver_id),
-    INDEX idx_messages_created_at (created_at)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE messages
+(
+    id          BIGINT AUTO_INCREMENT NOT NULL,
+    sender_id   BINARY(16)            NOT NULL,
+    receiver_id BINARY(16)            NOT NULL,
+    content     TEXT                  NOT NULL,
+    read_at     datetime              NULL,
+    created_at  datetime              NOT NULL,
+    CONSTRAINT pk_messages PRIMARY KEY (id)
+);
+
+CREATE INDEX idx_messages_receiver_read ON messages (receiver_id, read_at);
+
+CREATE INDEX idx_messages_receiver_sender_created ON messages (receiver_id, sender_id, created_at);
+
+CREATE INDEX idx_messages_sender_receiver_created ON messages (sender_id, receiver_id, created_at);
