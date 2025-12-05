6d25e4c19749edd41e930baf5255196731d72458
sillysillman <sillysillyman.cs@gmail.com>
2025-12-06 01:25:03 +0900

    refactor: 알림 도메인 필드 변경

--- src/main/kotlin/io/waggle/waggleapiserver/domain/notification/dto/request/NotificationCreateRequest.kt
+++ src/main/kotlin/io/waggle/waggleapiserver/domain/notification/dto/request/NotificationCreateRequest.kt
@@ -1,17 +1,10 @@
 package io.waggle.waggleapiserver.domain.notification.dto.request
 
 import io.waggle.waggleapiserver.domain.notification.NotificationType
+import java.util.UUID
 
 data class NotificationCreateRequest(
     val type: NotificationType,
-    val redirectUrl: String,
-    val contentArgs: List<String>,
-) {
-    companion object {
-        fun of(
-            type: NotificationType,
-            redirectUrl: String,
-            vararg contentArgs: String,
-        ): NotificationCreateRequest = NotificationCreateRequest(type, redirectUrl, contentArgs.asList())
-    }
-}
+    val projectId: Long?,
+    val userId: UUID,
+)
--- src/main/kotlin/io/waggle/waggleapiserver/domain/notification/dto/response/NotificationResponse.kt
+++ src/main/kotlin/io/waggle/waggleapiserver/domain/notification/dto/response/NotificationResponse.kt
@@ -2,32 +2,34 @@ package io.waggle.waggleapiserver.domain.notification.dto.response
 
 import io.swagger.v3.oas.annotations.media.Schema
 import io.waggle.waggleapiserver.domain.notification.Notification
+import io.waggle.waggleapiserver.domain.notification.NotificationType
+import io.waggle.waggleapiserver.domain.project.dto.response.ProjectSimpleResponse
 import java.time.Instant
 
 @Schema(description = "알림 응답 DTO")
 data class NotificationResponse(
     @Schema(description = "알림 ID", example = "1")
     val notificationId: Long,
-    @Schema(description = "알림 제목", example = "지원 승인")
-    val title: String,
-    @Schema(description = "알림 내용", example = "와글 프로젝트 지원이 승인되었습니다. 팀에 참여해 주세요!")
-    val content: String,
-    @Schema(description = "프로젝트 리다이렉트 URL", example = "https://waggle.store")
-    val redirectUrl: String,
+    @Schema(description = "알림 타입", example = "APPLICATION_RECEIVED")
+    val type: NotificationType,
+    @Schema(description = "프로젝트 정보")
+    val project: ProjectSimpleResponse?,
     @Schema(description = "알림 확인 일시", example = "false")
     val readAt: Instant?,
     @Schema(description = "알림 생성 일시", example = "2025-11-16T12:30:45.123456Z")
     val createdAt: Instant,
 ) {
     companion object {
-        fun from(notification: Notification): NotificationResponse =
+        fun of(
+            notification: Notification,
+            project: ProjectSimpleResponse?,
+        ): NotificationResponse =
             NotificationResponse(
-                notification.id,
-                notification.title,
-                notification.content,
-                notification.redirectUrl,
-                notification.readAt,
-                notification.createdAt,
+                notificationId = notification.id,
+                type = notification.type,
+                project = project,
+                readAt = notification.readAt,
+                createdAt = notification.createdAt,
             )
     }
 }
