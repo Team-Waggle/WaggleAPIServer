d68354b769ff5f0cab96597497bd5c9cda05acbe
sillysillyman <112466204+sillysillyman@users.noreply.github.com>
2026-02-03 00:45:13 +0900

    Update SecurityConfig.kt

--- src/main/kotlin/io/waggle/waggleapiserver/security/config/SecurityConfig.kt
+++ src/main/kotlin/io/waggle/waggleapiserver/security/config/SecurityConfig.kt
@@ -27,6 +27,7 @@ class SecurityConfig(
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
         http
+            .cors { it.configurationSource(corsConfigurationSource()) }
             .csrf { it.disable() }
             .sessionManagement {
                 it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -57,7 +58,7 @@ class SecurityConfig(
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("http://localhost:3000")
+        configuration.allowedOrigins = listOf("http://localhost:3000", "http://localhost:5173")
         configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
         configuration.allowedHeaders = listOf("*")
         configuration.allowCredentials = true
